6f4f0371f47532f093f93788c2f60eeffccdd3f2
Alexander von Below <Alex@vonBelow.Com>
2022-02-23 14:52:49 +0100

    Added a CarthageLib

---
+++ CarthageLib/CarthageLib/Carthage.swift
@@ -0,0 +1,14 @@
+//
+//  Carthage.swift
+//  CarthageLib
+//
+//  Created by Alexander v. Below on 23.02.22.
+//
+
+import Foundation
+
+public class Cart {
+    public func cartIdentifier() -> String {
+        return "Carthage"
+    }
+}
---
+++ CarthageLib/CarthageLib/CarthageLib.h
@@ -0,0 +1,18 @@
+//
+//  CarthageLib.h
+//  CarthageLib
+//
+//  Created by Alexander v. Below on 23.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageLib.
+FOUNDATION_EXPORT double CarthageLibVersionNumber;
+
+//! Project version string for CarthageLib.
+FOUNDATION_EXPORT const unsigned char CarthageLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageLib/PublicHeader.h>
+
+
